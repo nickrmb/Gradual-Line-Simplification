78aef0cf5a3ae182d7a773d8053105ed63a0528c
Nick K <nick@Nicks-Air.fritz.box>
2022-11-11 16:32:59 +0100

    Created Grabber and Simplifier for parsing gpx to spgx

---
+++ python-workspace/grabber.py
@@ -0,0 +1,47 @@
+import requests
+import re
+import simplifier
+
+graphsUrl = 'https://www.openstreetmap.org/traces/page/'
+
+def grab(fromPage, toPage, targetDirectory):
+
+    if (not targetDirectory.endswith('/')) and (len(targetDirectory) > 0): targetDirectory += '/'
+    
+    for i in range(fromPage, toPage + 1):
+        graphsRes = requests.get(graphsUrl + str(i))
+
+        matches = set(re.findall("/traces/[0-9]*", graphsRes.text))
+
+        for m in matches:
+            split = m.split('/')
+            trace = split[2]
+            if len(trace) != 7: continue
+
+            downloadUrl = 'https://www.openstreetmap.org/trace/' + str(trace) + '/data';
+
+            downloadRes = requests.get(downloadUrl)
+
+            out = open(targetDirectory + str(trace) + ".sgpx", "w")
+
+            data = str(downloadRes.content)
+
+            simplified = simplifier.simplifyData(data)
+            
+            length = len(simplified)
+
+            for i in range(length):
+                point = simplified[i]
+                line = str(point[0]) + ',' + str(point[1])
+                out.write(line)
+
+                if (i == length-1):
+                    continue
+                out.write('\n')
+
+            out.close
+
+    
+grab(1, 1, "/Users/nick/Documents/University/Bachelor Project/gradual-line-simplification/python-workspace/data")
+
+    
---
+++ python-workspace/simplifier.py
@@ -0,0 +1,48 @@
+import re
+
+
+def simplifyData(data):
+    points = re.findall("< *trkpt *lat=\"-?[0-9]*\.[0-9]*\" *lon=\"-?[0-9]*\.[0-9]*\" *>", data)
+
+    simplified = []
+    
+    for point in points:
+        split = point.split('\"')
+        lat = split[1]
+        lon = split[3]
+
+        simplified.append((lat,lon))
+    
+    return simplified
+
+
+
+def simplifyFile(filePath):
+    if (not filePath.endswith('.gpx')):
+        print('Not a gpx file!')
+        return
+    
+    data = ''
+
+    with open(filePath, 'r') as file:
+        data = file.read()
+
+    simplified = simplifyData(data)
+
+    outputFile = filePath[:-3] + 'sgpx'
+
+    out = open(outputFile, "w")
+
+    length = len(simplified)
+
+    for i in range(length):
+        point = simplified[i]
+        out.write(str(point[0]) + ',' + str(point[1]))
+        
+        if (i == length-1): continue
+        out.write('\n')
+    
+    out.close
+
+
+simplifyFile('/Users/nick/Documents/University/Bachelor Project/gradual-line-simplification/python-workspace/5326431.gpx')
